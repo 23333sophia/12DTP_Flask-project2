36b9d8ae9d0c7dac295a2714a7e0695d9a602790
Sophia Kim <23333@burnside.school.nz>
2026-02-17 08:59:40 +1300

    importing sql

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
 
+DATABASE = 'boynextdoordb.db'
+
+#initialising app
 app = Flask(__name__)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 @app.route("/")
 def home():
-    return "Hello Flask!"
+    #home page
+    db = get_db()
+    cursor = db.cursor()
+    sql = "SELECT * FROM songs;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return str(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
